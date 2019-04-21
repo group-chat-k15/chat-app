ffab048013cffd89fe02895412bd6692f26cffa0
vanhao122995 <vuvanhao122995@gmail.com>
2019-04-22 05:27:17 +0700

    lần 10

--- application/models/Add_friend_m.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-Class Add_friend_m extends MY_Model {
-
-    var $table = 'add_friend';
-
-}
--- public/js/layout.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$('#action_menu_btn').click(function(){
-		$('.action_menu').toggle();
-	});
-	
-	$('.my-profile').click(() => {
-		$('.my-profile-zone').toggleClass('active');
-	});
-});
